397676efbbe5bac9b864daa5d4e849d658f27e36
Tom Dooner <tom.dooner@brigade.com>
2016-12-09 18:45:55 -0500

    Modify problem 3 solution to handle vertical rows
    This could probably be solved more simply by representing the input as
    a matrix, but whatever, this took me about as much time to implement
    probably!

--- 03/process.rb
+++ 03/process.rb
@@ -1,9 +1,34 @@
 valid_count = 0
 
-ARGF.each_line.map(&:strip).map(&:split).each do |first, second, third|
-  min, med, max = [first.to_i, second.to_i, third.to_i].sort
+class VerticalReader
+  def initialize(file)
+    @contents = File.read(file)
+  end
 
-  valid_count += 1 if (min + med > max)
+  def each_triangle(&block)
+    triangles = [[], [], []]
+
+    @contents.each_line do |line|
+      triangle_one, triangle_two, triangle_three = line.strip.split
+      triangles[0] << triangle_one
+      triangles[1] << triangle_two
+      triangles[2] << triangle_three
+
+      if triangles[0].length == 3
+        block.call(*triangles[0])
+        block.call(*triangles[1])
+        block.call(*triangles[2])
+
+        triangles.map(&:clear)
+      end
+    end
+  end
 end
+
+VerticalReader.new(ARGV[0])
+  .each_triangle do |first, second, third|
+    min, med, max = [first.to_i, second.to_i, third.to_i].sort
+    valid_count += 1 if (min + med > max)
+  end
 
 puts valid_count
