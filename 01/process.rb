ccf761ee0b61d6e89de7552a2cd7b0395842d52d
Tom Dooner <tom.dooner@brigade.com>
2016-12-09 17:11:25 -0500

    Solve problem 1 part 2
    This requires tracking locations and printing the distance of the first
    duplicate location. For this I use a simple Set and an instance variable
    that's initialized to the first offset once a location is visited twice.

--- 01/process.rb
+++ 01/process.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Traveler
   NORTH = :north
   SOUTH = :south
@@ -12,13 +14,15 @@ class Traveler
 
   def initialize(direction)
     @direction = direction
+    @history = Set.new
     @offset = { x: 0, y: 0 }
+    @first_repeat = nil
   end
 
   # @param {String} move In format "L1" signifying "turn left and walk one
   #   block"
   def move(move)
-    # first, turn
+    # first, turn in preparation for the move
     case move[0]
     when 'L'
       @direction = DIRECTIONS[@direction][:left]
@@ -28,28 +32,70 @@ class Traveler
 
     # then, walk that many blocks
     distance = move[1..-1].to_i
-    case @direction
-    when NORTH
-      @offset[:y] += distance
-    when SOUTH
-      @offset[:y] -= distance
-    when EAST
-      @offset[:x] += distance
-    when WEST
-      @offset[:x] -= distance
+    distance.times do
+      case @direction
+      when NORTH
+        @offset[:y] += 1
+      when SOUTH
+        @offset[:y] -= 1
+      when EAST
+        @offset[:x] += 1
+      when WEST
+        @offset[:x] -= 1
+      end
+
+      if !@first_repeat && has_visited?(*@offset.values_at(:x, :y))
+        @first_repeat = @offset.dup
+      end
+
+      mark_visited(*@offset.values_at(:x, :y))
     end
   end
 
-  def distance
-    @offset[:x].abs + @offset[:y].abs
+  def distance(x = @offset[:x], y = @offset[:y])
+    x.abs + y.abs
+  end
+
+  def distance_of_first_duplicate_location
+    return nil unless @first_repeat
+    distance(*@first_repeat.values_at(:x, :y))
+  end
+
+  private
+
+  def has_visited?(x, y)
+    @history.include?([x, y])
+  end
+
+  def mark_visited(x, y)
+    # save the (x, y) coordinate into an array of visited locations so we can
+    # tell if we pass this coordinate again later
+    @history << [x, y]
   end
 end
 
-traveler = Traveler.new(Traveler::NORTH)
-
-input_file = File.expand_path('../input', __FILE__)
-File.read(input_file).split(',').map(&:strip).each do |move|
-  traveler.move(move)
+def process_input(input_file)
+  File.read(input_file).split(',').map(&:strip)
 end
 
-puts traveler.distance
+def run(moves)
+  traveler = Traveler.new(Traveler::NORTH)
+
+  moves.each do |move|
+    traveler.move(move)
+  end
+
+  {
+    part_one: traveler.distance,
+    part_two: traveler.distance_of_first_duplicate_location,
+  }
+end
+
+# first run it on the test data
+raise unless 12 == run(%w[R5 L5 R5 R3])[:part_one]
+raise unless 2 == run(%w[R2 R2 R2])[:part_one]
+raise unless 4 == run(%w[R8 R4 R4 R8])[:part_two]
+
+# then run it for realz
+answers = run(process_input(File.expand_path('../input', __FILE__)))
+puts answers.inspect
