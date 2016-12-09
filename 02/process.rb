7e7266d5a81cb08b82400fe06269bbeaae01de1d
Tom Dooner <tom.dooner@brigade.com>
2016-12-09 18:29:13 -0500

    Add files for problem 2, part 2
    This required modularizing the map layout. I did this by processing a
    text file and restricting movement to any coordinate that is a space or
    off the end of the line. This allows the creation of arbitrary shapes.

--- 02/process.rb
+++ 02/process.rb
@@ -1,33 +1,76 @@
 class Keypad
   attr_reader :code
 
-  def initialize
-    @button = 5
+  START_BUTTON = '5'
+
+  def initialize(description)
+    @description = description
     @code = ''
+
+    # determine initial button placement (row/col) because it's hardcoded:
+    # always start on "5"
+    @description.each_with_index do |line, row|
+      col = line.index(START_BUTTON)
+
+      if col
+        @button = { row: row, col: col }
+        break
+      end
+    end
   end
 
   # @param {String} movements Series of movements from the previous button
   def process_movements(movements)
-    movements.each_char do |char|
-      case char
-      when 'U'
-        @button -= 3 if @button > 3
-      when 'D'
-        @button += 3 if @button <= 6
-      when 'L'
-        @button -= 1 if @button % 3 != 1
-      when 'R'
-        @button += 1 if @button % 3 != 0
+    movements.each_char do |move|
+      next_button = next_coordinates(move)
+
+      if valid_location?(next_button)
+        @button = next_button
       end
     end
 
-    @code += @button.to_s
+    @code += at(@button[:row], @button[:col])
+  end
+
+  private
+
+  def at(row, col)
+    return nil if row < 0 # overflowed above the board
+    return nil if row >= @description.length # overflowed below the board
+    return nil if col < 0 # overflowed left of board
+    return nil if col >= @description[row].length # overflowed right of board
+
+    value = @description[row][col]
+
+    return nil if value.nil? # overflowed left of board
+    return nil if value == ' ' # overflowed left of board
+
+    value
+  end
+
+  def valid_location?(next_button)
+    !at(next_button[:row], next_button[:col]).nil?
+  end
+
+  def next_coordinates(direction)
+    @button.dup.tap do |next_button|
+      case direction
+      when 'U'
+        next_button[:row] -= 1
+      when 'D'
+        next_button[:row] += 1
+      when 'L'
+        next_button[:col] -= 1
+      when 'R'
+        next_button[:col] += 1
+      end
+    end
   end
 end
 
-k = Keypad.new
+k = Keypad.new(File.read(ARGV.shift).each_line.map(&:rstrip))
 
-ARGF.each_line.map(&:strip).each do |line|
+File.read(ARGV.shift).each_line.map(&:strip).each do |line|
   k.process_movements(line)
 end
 
--- Makefile
+++ Makefile
@@ -4,5 +4,6 @@ all: 01/answer 02/answer
 	ruby 01/process.rb > $@
 
 02/answer: 02/process.rb 02/input-test 02/input
-	ruby 02/process.rb 02/input-test | grep -E '1985'
-	ruby 02/process.rb 02/input > $@
+	ruby 02/process.rb 02/input-test-description 02/input-test | grep -E '1985'
+	ruby 02/process.rb 02/input-description 02/input-test | grep -E '5DB3'
+	ruby 02/process.rb 02/input-description 02/input > $@
