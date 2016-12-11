b9c6479da6b11f112c7339274b21989bf041acac
Tom Dooner <tom.dooner@brigade.com>
2016-12-10 22:03:00 -0500

    Solve problem 7 part 2
    This gave me some trouble because of some unfamiliarity with enumerable
    which made it hard to track down some incorrect BABs.

--- 07/process.rb
+++ 07/process.rb
@@ -1,41 +1,76 @@
+require 'set'
+
 class IPv7Address
   def self.from_string(str)
     negate = false
     supports_tls = false
+    negates_tls = false
+    abas = Set.new
 
+    # during the first scan, determine if the IP supports "TLS" while also
+    # keeping track of all "ABA"s
     str.scan(/[a-z]+|\[|\]/).each do |part|
       if part == '[' || part == ']'
         negate = part == '['
         next
       end
 
+      if !negate
+        abas |= part.each_char.each_cons(3).map(&:join)
+      end
+
       part.each_char.each_cons(4) do |a, b, c, d|
         next unless a == d && b == c && a != b
 
         supports_tls = true unless negate
-
-        # an abba in a negative space overrides anything else
-        return new(false) if negate
+        negates_tls = true if negate
       end
     end
 
-    new(supports_tls)
+    # then, parse the string again to determine if the IP supports "SSL" if we
+    # are in a negation block and a 3-letter sequence matches a previous "ABA"
+    negate = false
+    supports_ssl = false
+    str.scan(/[a-z]+|\[|\]/).each do |part|
+      if part == '[' || part == ']'
+        negate = part == '['
+        next
+      end
+
+      if negate
+        babs = part.each_char.each_cons(3)
+          .find_all { |a, b, c| a == c && b != c }
+          .map { |a, b, _| "#{b}#{a}#{b}" }
+        supports_ssl |= !(abas & babs).empty?
+      end
+    end
+
+    new(supports_tls && !negates_tls, supports_ssl)
   end
 
-  def initialize(supports_tls)
+  def initialize(supports_tls, supports_ssl)
     @supports_tls = supports_tls
+    @supports_ssl = supports_ssl
   end
 
   def supports_tls?
     @supports_tls
   end
+
+  def supports_ssl?
+    @supports_ssl
+  end
 end
 
-valid = 0
+valid_tls = 0
+valid_ssl = 0
+
 ARGF.each_line do |address|
   a = IPv7Address.from_string(address)
 
-  valid += 1 if a.supports_tls?
+  valid_tls += 1 if a.supports_tls?
+  valid_ssl += 1 if a.supports_ssl?
 end
 
-puts valid
+puts "Num supporting TLS (part one): #{valid_tls}"
+puts "Num supporting SSL (part two): #{valid_ssl}"
