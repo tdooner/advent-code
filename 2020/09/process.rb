7263663377e098932c765dd7d0c7a8af12712dd6
Tom Dooner <tomdooner@gmail.com>
2020-12-09 00:21:09 -0500

    Refactor 2020/09
    Use a bit more ruby-idiomatic looping constructs.

--- 2020/09/process.rb
+++ 2020/09/process.rb
@@ -3,33 +3,29 @@ def process_line(line)
 end
 
 def solve(input)
-  preamble = input.first(25)
-  input[25..-1].find do |num|
-    invalid = preamble.combination(2).none? { |n1, n2| n1 + n2 == num }
-    preamble.shift
-    preamble.push(num)
-    invalid
-  end
+  input.each_with_index.find do |num, i|
+    next if i < 25
+
+    input[(i - 25)..i]
+      .combination(2)
+      .none? { |n1, n2| n1 + n2 == num }
+  end.first
 end
 
 def solve2(input, target)
-  cons = 2
-  found = nil
-  while cons < input.length && !found
-    found = input.each_cons(cons).find do |slice|
-      slice.sum == target
+  (2..input.length).each do |length|
+    input.each_cons(length).find do |slice|
+      next unless slice.sum == target
+      return slice.min + slice.max
     end
-    cons += 1
   end
-
-  found.min + found.max
 end
 
 input = File.read(ARGV[0]).split(/\n/)
   .map { |l| process_line(l) }
 
 puts 'Part 1:'
-part1 = solve(input)
+puts (part1 = solve(input))
 
 puts 'Part 2:'
 puts solve2(input, part1)
