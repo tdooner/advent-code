0cc256bbcae05addc7879c905dfaba5e74d38d19
Tom Dooner <tomdooner@gmail.com>
2020-12-09 00:59:19 -0500

    Code Golf 2020/09 down 3 more characters
    Going to bed now.
    
    ```
    » wc -c process.rb
         204 process.rb
    ```

--- 2020/09/process.rb
+++ 2020/09/process.rb
@@ -1,4 +1,4 @@
 a=ARGF.readlines.map(&:to_i)
-p(o=a[(25..a.length).find{|i|!a[(i-25)..i].combination(2).map(&:sum).include?(a[i])}])
-def z(a,t,l=2);r=a.each_cons(l).find{|m|m.sum==t};r&&r.min+r.max||z(a,t,l+1);end
+p(o=a[(25..a.length).find{|i|!a[i-25..i].combination(2).map(&:sum).include?(a[i])}])
+def z(a,t,l=2);a.each_cons(l){|m|return m.min+m.max if m.sum==t};z(a,t,l+1);end
 p(z(a,o))
