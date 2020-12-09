e74b7d47785b0832b0c1300854b3f47c0eaffdac
Tom Dooner <tomdooner@gmail.com>
2020-12-09 12:37:07 -0500

    Code golf 2020/09 down to 188 chars
    Woah. Thanks StackOverflow for the tips.

--- 2020/09/process.rb
+++ 2020/09/process.rb
@@ -1,4 +1,4 @@
-a=ARGF.readlines.map(&:to_i)
+a=$<.map(&:to_i)
 p(o=a[(25..a.length).find{|i|!a[i-25..i].combination(2).map(&:sum).include?(a[i])}])
-def z(a,t,l=2);a.each_cons(l){|m|return m.min+m.max if m.sum==t};z(a,t,l+1);end
-p(z(a,o))
+z=->(a,t,l=2){a.each_cons(l){|m|return m.min+m.max if m.sum==t};z[a,t,l+1]}
+p(z[a,o])
