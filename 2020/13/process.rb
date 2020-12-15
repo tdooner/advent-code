dbbd4b59f7fd4793a0882b7c6495245a3194fd75
Tom Dooner <tomdooner@gmail.com>
2020-12-15 01:33:19 -0500

    Solve 2020/13 Part 2
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     13   00:07:19    833      0       >24h  25834      0
    
    Math is hard. I knew it was the CRT but I missed, on the day of the
    problem, that there was a unique solution (i.e. the skipped X's shifted
    the timetable down by exactly 1... not >= 1).
    
    I stole the invmod and extended_gcd implementation because I want to go
    to bed.

--- 2020/13/process.rb
+++ 2020/13/process.rb
@@ -18,8 +18,49 @@ def process(input)
   (earliest - start) * bus_id
 end
 
+# stolen from rosetta code
+def extended_gcd(a, b)
+  last_remainder, remainder = a.abs, b.abs
+  x, last_x, y, last_y = 0, 1, 1, 0
+  while remainder != 0
+    last_remainder, (quotient, remainder) = remainder, last_remainder.divmod(remainder)
+    x, last_x = last_x - quotient*x, x
+    y, last_y = last_y - quotient*y, y
+  end
+
+  return last_remainder, last_x * (a < 0 ? -1 : 1)
+end
+
+def invmod(e, et)
+  g, x = extended_gcd(e, et)
+  if g != 1
+    raise 'The maths are broken!'
+  end
+  x % et
+end
+
+# Implemented based on this page:
+# https://brilliant.org/wiki/chinese-remainder-theorem/
+def chinese_remainder_theorem(a, n)
+  big_n = n.reduce(1, :*)
+  y = n.map { |n_i| big_n / n_i }
+  z = y.zip(n).map { |y_i, n_i| invmod(y_i, n_i) }
+
+  a.zip(y, z).sum { |a_i, y_i, z_i| a_i * y_i * z_i } % big_n
+end
+
 def earliest_for_all(input)
-  # give up for now
+  start = input.shift.to_i
+  buses = input[0].split(',').each_with_index.find_all { |bus, i| bus != 'x' }.map { |bus, i| [bus.to_i, i] }
+
+  puts 'Chinese Remainder Theorem:'
+  buses = buses.map do |bus, time_offset|
+    puts "x = #{(bus - time_offset) % bus} mod #{bus}"
+
+    [(bus - time_offset) % bus, bus]
+  end
+
+  puts chinese_remainder_theorem(buses.map(&:first), buses.map(&:last))
 end
 
 puts 'Part 1:'
