928d1a71244d2411087e881aee042fd664d5c228
Tom Dooner <tomdooner@gmail.com>
2022-12-24 19:45:28 -0500

    Add 2022/19 Part 2
    * Update algorithm to only explore states where we buy things.
    
    First part is down to ~18 sec, second part ~62 sec.

--- 2022/19/process.rb
+++ 2022/19/process.rb
@@ -5,10 +5,11 @@ def debug(&block); puts block.call if ENV['D'].present?; end
 Blueprint = Struct.new(:i, :ore_cost, :clay_ore_cost, :obsidian_ore_cost, :obsidian_clay_cost, :geode_ore_cost, :geode_obsidian_cost)
 INITIAL_ROBOTS = { ore: 1, clay: 0, obsidian: 0, geode: 0 }
 INITIAL_RESOURCES = { ore: 0, clay: 0, obsidian: 0, geode: 0 }
+$FINAL_TURN = 24
 
 class Hash
-  def increment_by(hash)
-    self.dup.tap { |h| hash.each { |k, v| h[k] += v } }
+  def increment_by(hash, times: 1)
+    self.dup.tap { |h| hash.each { |k, v| h[k] += (v * times) } }
   end
 
   def increment(key, n = 1)
@@ -20,50 +21,97 @@ class Hash
   end
 end
 
-def filter_states(states)
-  # remove any states that, if we do the greediest thing (buying a geode robot
-  # every turn) will still be inferior to another known state where we do
-  # nothing.
-  geodes_when_we_do_nothing = states.map(&:min_endgame_score).max
-
-  states.keep_if { |s| geodes_when_we_do_nothing < s.max_endgame_score }
-end
-
 State = Struct.new(:blueprint, :turn, :robots, :resources) do
+
   def max_endgame_score
     # assume we buy geode robot each remaining turn? so on turn 21, the score
     # will be the current number plus 3 for the geode robot that comes online on
     # turn 22, 2 for the robot on turn 23, and 1 for the robot on 24.
-    remaining_turns = (24 - turn) # 24 - 21 = 3
+    remaining_turns = ($FINAL_TURN - turn) # 24 - 21 = 3
 
-    min_endgame_score + (remaining_turns * (remaining_turns + 1) / 2)
+    endgame_score + (remaining_turns * (remaining_turns + 1) / 2)
   end
 
-  def min_endgame_score
-    # assume we do nothing, how many geodes will we have?
-    remaining_turns = (24 - turn)
+  def endgame_score
+    remaining_turns = ($FINAL_TURN - turn + 1)
 
-    resources[:geode] + robots[:geode] * remaining_turns
+    resources[:geode] + (robots[:geode] * remaining_turns)
   end
 
   def possibilities
     [].tap do |p|
       next_resources = resources.increment_by(robots)
 
-      p << State.new(blueprint, turn + 1, robots, next_resources)
-      if resources[:ore] >= blueprint.ore_cost
-        p << State.new(blueprint, turn + 1, robots.increment(:ore), next_resources.decrement(:ore, blueprint.ore_cost))
+      # save up for the next available ore bot
+      turns_to_wait = [0, ((blueprint.ore_cost - resources[:ore].to_f) / robots[:ore]).ceil].max
+      following_turn = turn + turns_to_wait + 1
+      if following_turn <= $FINAL_TURN
+        p << State.new(
+          blueprint,
+          following_turn,
+          robots.increment(:ore),
+          next_resources.decrement(:ore, blueprint.ore_cost).increment_by(robots, times: turns_to_wait)
+        )
       end
-      if resources[:ore] >= blueprint.clay_ore_cost
-        p << State.new(blueprint, turn + 1, robots.increment(:clay), next_resources.decrement(:ore, blueprint.clay_ore_cost))
+
+      # save up for the next clay bot
+      turns_to_wait = [0, ((blueprint.clay_ore_cost - resources[:ore].to_f) / robots[:ore]).ceil].max
+      following_turn = turn + turns_to_wait + 1
+      if following_turn <= $FINAL_TURN
+        p << State.new(
+          blueprint,
+          following_turn,
+          robots.increment(:clay),
+          next_resources.decrement(:ore, blueprint.clay_ore_cost).increment_by(robots, times: turns_to_wait)
+        )
       end
-      if resources[:ore] >= blueprint.obsidian_ore_cost && resources[:clay] >= blueprint.obsidian_clay_cost
-        r = next_resources.decrement(:ore, blueprint.obsidian_ore_cost).decrement(:clay, blueprint.obsidian_clay_cost)
-        p << State.new(blueprint, turn + 1, robots.increment(:obsidian), r)
+
+      # save up for obsidian bot
+      if robots[:clay] >= 1
+        turns_to_wait = [
+          0,
+          ((blueprint.obsidian_ore_cost - resources[:ore]) / robots[:ore].to_f).ceil,
+          ((blueprint.obsidian_clay_cost - resources[:clay]) / robots[:clay].to_f).ceil
+        ].max
+        following_turn = turn + turns_to_wait + 1
+
+        if following_turn <= $FINAL_TURN
+          r = next_resources
+            .decrement(:ore, blueprint.obsidian_ore_cost)
+            .decrement(:clay, blueprint.obsidian_clay_cost)
+            .increment_by(robots, times: turns_to_wait)
+
+          p << State.new(
+            blueprint,
+            following_turn,
+            robots.increment(:obsidian),
+            r
+          )
+        end
       end
-      if resources[:ore] >= blueprint.geode_ore_cost && resources[:obsidian] >= blueprint.geode_obsidian_cost
-        r = next_resources.decrement(:ore, blueprint.geode_ore_cost).decrement(:obsidian, blueprint.geode_obsidian_cost)
-        p << State.new(blueprint, turn + 1, robots.increment(:geode), r)
+
+      # save up for geode bot
+      if resources[:obsidian] >= 1
+        turns_to_wait = [
+          0,
+          ((blueprint.geode_ore_cost - resources[:ore]) / robots[:ore].to_f).ceil,
+          ((blueprint.geode_obsidian_cost - resources[:obsidian]) / robots[:obsidian].to_f).ceil
+        ].max
+        following_turn = turn + turns_to_wait + 1
+
+        if following_turn <= $FINAL_TURN
+          r = next_resources
+            .decrement(:ore, blueprint.geode_ore_cost)
+            .decrement(:obsidian, blueprint.geode_obsidian_cost)
+            .increment_by(robots, times: turns_to_wait)
+
+          p << State.new(
+            blueprint,
+            following_turn,
+            robots.increment(:geode),
+            r
+          )
+        end
       end
     end
   end
@@ -81,22 +129,50 @@ end
 
 def part1(blueprints)
   blueprints.sum do |blueprint|
+    debug { "Blueprint #{blueprint.i}" }
     states = [State.new(blueprint, 1, INITIAL_ROBOTS, INITIAL_RESOURCES)]
-    loop do
-      debug { "  Beginning turn #{states.first.turn} - length: #{states.length}" }
-      break if states.first.turn == 24
+    max_geodes = 0
 
-      states = filter_states(states).flat_map(&:possibilities).uniq
+    while states.any?
+      debug { "  Next step: #{states.length} states. Current best: #{max_geodes}" }
+
+      max_geodes = [max_geodes, states.map(&:endgame_score).max].max
+      next_states = filter_states(states, max_geodes).uniq.flat_map(&:possibilities)
+      states = next_states
     end
 
-    max_geodes = states.map { |s| s.resources[:geode] + s.robots[:geode] }.max
-    debug { "  Blueprint #{blueprint.i} - Max Geodes: #{max_geodes}" }
+    debug { "  Max Geodes: #{max_geodes}" }
 
     max_geodes * blueprint.i
   end
 end
 
+def filter_states(states, best_score)
+  # remove any states that, if we do the greediest thing (buying a geode robot
+  # every turn) will still be inferior to the best known state
+  states.keep_if { |s| best_score < s.max_endgame_score }
+end
+
 def part2(blueprints)
+  $FINAL_TURN = 32
+
+  blueprints.first(3).map do |blueprint|
+    debug { "Blueprint #{blueprint.i}" }
+    states = [State.new(blueprint, 1, INITIAL_ROBOTS, INITIAL_RESOURCES)]
+    max_geodes = 0
+
+    while states.any?
+      debug { "  Next step: #{states.length} states. Current best: #{max_geodes}" }
+
+      max_geodes = [max_geodes, states.map(&:endgame_score).max].max
+      next_states = filter_states(states, max_geodes).uniq.flat_map(&:possibilities)
+      states = next_states
+    end
+
+    debug { "  Max Geodes: #{max_geodes}" }
+
+    max_geodes
+  end.reduce(:*)
 end
 
 parsed = parse(ARGF.read.chomp)
