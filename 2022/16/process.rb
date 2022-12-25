e4b9cb9685f4f2979619273446447ccd31a47c6c
Tom Dooner <tomdooner@gmail.com>
2022-12-24 23:06:48 -0500

    Finish 2022/16 with searching from the 19th
    It finishes in ~16 sec - not bad.

--- 2022/16/process.rb
+++ 2022/16/process.rb
@@ -1,6 +1,6 @@
 require 'set'
 require 'bundler'; Bundler.require(:default)
-def pbcopy(value); return if value.nil?; `/bin/bash -c 'echo -n "#{value}" | pbcopy'`; "#{value}    <- COPIED TO CLIPBOARD" end
+def pbcopy(&block); start = Time.now; value = block.call; return if value.nil?; `/bin/bash -c 'echo -n "#{value}" | pbcopy'`; puts "#{value}    <- COPIED TO CLIPBOARD (in #{Time.now - start}s)"; end
 def debug(&block); puts block.call if ENV['D'].present?; end
 
 def parse(input)
@@ -10,42 +10,51 @@ def parse(input)
     .map { |l| [l[1], l[4].split('=').last.to_i, l[9..].map { |v| v.gsub(',', '') }] }
 end
 
-def max_flow(tunnels, valves, enabled = {}, position = 'AA', minute = 0, path = [])
-  movement_options = tunnels[position].shuffle
+# Attempt at abstracting a common BFS-based problem solver
+class BFSSearcher
+  def initialize(state, end_turn)
+    @states = [state]
+    @end_turn = end_turn
+  end
 
-  if minute == 30 || enabled.length == valves.length
-    max = enabled.filter_map { |valve, minute| (30 - enabled[valve] + 1) * valves[valve] if enabled[valve] }.sum
-    if max > 200
-      puts path.join(", ")
-      puts max
+  def maximize(method, filter_method: nil)
+    current_max = 0
+
+    while @states.any?
+      debug { "  Current step: #{@states.length} states. Current best: #{current_max}" }
+
+      current_max = [current_max, @states.map { |s| s.send(method, @end_turn) }.max].max
+      @states = @states.keep_if { |s| s.send(filter_method, @end_turn) >= current_max } if filter_method
+      next_states = @states.flat_map(&:possibilities)
+      @states = next_states
     end
-    return max
+
+    debug { "  Final Max: #{current_max}" }
+    current_max
   end
-
-  max = 0
-  if enabled[position].nil?
-    max = max_flow(tunnels, valves, enabled.dup.tap { |e| e[position] = minute + 1 }, position, minute + 1, path.dup.append("Enable " + position))
-  end
-
-  max_by_moving = movement_options.map { |m| max_flow(tunnels, valves, enabled, m, minute + 1, path.dup.append("Move " + m)) }.max
-  max = max_by_moving if max < max_by_moving
-
-  max
 end
 
-def max_flow2(tunnels, valves, paths, position: 'AA', minute: 1, enabled: {})
-  # heuristic: try to get to the best valves first
-  best_valves = (valves.keys - enabled.keys).filter_map { |v| [v, valves[v] * (30 - paths[[position, v]])] if v != position }.sort_by(&:last).reverse
-  if best_valves.none? && minute <= 30
-    binding.pry if enabled['DD'] == 2
-    return enabled.filter_map { |valve, minute| (30 - enabled[valve]) * valves[valve] }.sum
+State = Struct.new(:turn, :valves, :paths, :enabled_valves, :position) do
+  def possibilities
+    (valves.keys - enabled_valves.keys).map do |valve_to_enable|
+      next_turn = turn + paths[[position, valve_to_enable]] + 1
+
+      State.new(next_turn, valves, paths, enabled_valves.dup.tap { |h| h[valve_to_enable] = next_turn }, valve_to_enable)
+    end
   end
 
-  best_valves.map do |valve, _|
-    distance = paths[[position, valve]]
+  def total_flow(end_turn)
+    enabled_valves.sum do |valve, turn_enabled|
+      (end_turn - turn_enabled + 1) * valves[valve]
+    end
+  end
 
-    max_flow2(tunnels, valves, paths, position: valve, minute: minute + distance, enabled: enabled.merge(valve => minute + distance)) || 0
-  end.max
+  def max_total_flow(end_turn)
+    # if all remaining pipes were opened immediately, how much flow they'd have
+    total_flow(end_turn) + (valves.keys - enabled_valves.keys).sum do |valve_to_enable|
+      (end_turn - turn + 1) * valves[valve_to_enable]
+    end
+  end
 end
 
 def shortest_path(tunnels, src, dest, distance = 0, visited = [])
@@ -57,22 +66,25 @@ end
 def part1(parsed)
   tunnels = parsed.map { |src, _flow, *tunnels| [src, tunnels.flatten] }.to_h
   valves = parsed.map { |src, flow| [src, flow] }.to_h
+  debug { "Computing shortest paths..." }
   paths = valves.keys.permutation(2).map { |src, dest| [[src, dest], shortest_path(tunnels, src, dest)] }.to_h
   enabled = valves.filter_map { |k, v| [k, 0] if v == 0 }.to_h
-  binding.pry
 
-  max_flow2(tunnels, valves, paths, enabled: enabled)
+  start = State.new(1, valves, paths, enabled, 'AA')
+  BFSSearcher
+    .new(start, 30)
+    .maximize(:total_flow, filter_method: :max_total_flow)
 end
 
 def part2(parsed)
 end
 
 input = ARGF.read.chomp
-puts "PARSED: #{parse(input)}"
+#puts "PARSED: #{parse(input)}"
 
 puts "Part 1:"
-puts pbcopy(part1(parse(input)))
+pbcopy { part1(parse(input)) }
 
 puts
 puts "Part 2:"
-puts pbcopy(part2(parse(input)))
+pbcopy { part2(parse(input)) }
